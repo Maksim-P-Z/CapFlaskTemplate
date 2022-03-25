8b7a625cf907e8f27178e1284a258a17dfb5a2ff
Maksim <maksim.bigz13@gmail.com>
2022-03-25 12:46:00 -0700

    slowly but surely

---
+++ app/routes/fact.py
@@ -0,0 +1,12 @@
+
+from app import app, login
+import mongoengine.errors
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user
+from app.classes.data import Fact
+from app.classes.forms import FactForm
+from flask_login import login_required
+import datetime as dt
+
+
+
